3c46e7c83f253b1dfefdf3740671286c0c5067ee
rootkc <rootkc@protonmail.com>
2023-04-11 18:10:43 +0200

    get live order book from bitstamp

---
+++ src/bitstamp.rs
@@ -0,0 +1,88 @@
+use anyhow::Result;
+use futures_util::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+const BITSTAMP_WEB_SOCKET_URL: &str = "wss://ws.bitstamp.net/";
+
+pub struct Bitstamp {}
+
+impl Bitstamp {
+    pub async fn get_order_book() -> Result<()> {
+        let (ws_stream, _) = connect_async(BITSTAMP_WEB_SOCKET_URL).await?;
+        let (mut write, read) = ws_stream.split();
+
+        write
+            .send(Message::Text(
+                BitstampSubscription::new("bts:subscribe", "order_book_btcusd").to_json(),
+            ))
+            .await?;
+
+        read.for_each(|message| async {
+            if let Ok(Message::Text(text)) = message {
+                let response_event: BitstampResponseEvent = serde_json::from_str(&text).unwrap();
+                if let BitstampWebSocketEvent::Data = response_event.event {
+                    let response: BitstampResponse = serde_json::from_str(&text).unwrap();
+                    println!("response: {:#?}", response.data.bids.len());
+                }
+            }
+        })
+        .await;
+
+        Ok(())
+    }
+}
+
+// Channel subscriptions
+#[derive(Serialize)]
+struct BitstampSubscriptionData {
+    channel: String,
+}
+
+#[derive(Serialize)]
+struct BitstampSubscription {
+    event: String,
+    data: BitstampSubscriptionData,
+}
+
+impl BitstampSubscription {
+    fn new(event: &str, channel: &str) -> Self {
+        Self {
+            event: event.to_string(),
+            data: BitstampSubscriptionData {
+                channel: channel.to_string(),
+            },
+        }
+    }
+    fn to_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+}
+
+// Response types
+type BitstampBidAskType = Vec<[String; 2]>;
+
+#[derive(Deserialize, Debug)]
+struct BitstampResponseData {
+    timestamp: String,
+    microtimestamp: String,
+    bids: BitstampBidAskType,
+    asks: BitstampBidAskType,
+}
+
+#[derive(Deserialize, Debug)]
+struct BitstampResponse {
+    data: BitstampResponseData,
+}
+
+#[derive(Deserialize, Debug)]
+enum BitstampWebSocketEvent {
+    #[serde(rename = "bts:subscription_succeeded")]
+    BtsSubscriptionSucceded,
+    #[serde(rename = "data")]
+    Data,
+}
+#[derive(Deserialize, Debug)]
+struct BitstampResponseEvent {
+    event: BitstampWebSocketEvent,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod bitstamp;
+use crate::bitstamp::Bitstamp;
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Bitstamp::get_order_book().await?;
+    Ok(())
 }
